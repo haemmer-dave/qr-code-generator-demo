e32d6c5b69310df37658464983214baecb2c29f5
David Hämmerle <12235946+haemmer-dave@users.noreply.github.com>
2021-10-16 15:41:05 +0200

    Adding exception handling

--- src/index.js
+++ src/index.js
@@ -4,14 +4,18 @@ const qrContent = document.getElementById('qr-content');
 const canvas = document.getElementById('qr-output');
 const submitButton = document.getElementById('generate-button');
 
-submitButton.addEventListener('click', function () {
+submitButton.addEventListener('click', function (event) {
   let qrCode = new QrCodeWithLogo({
     canvas: canvas,
     content: qrContent.value,
     width: 380,
     image: document.getElementById('image'),
-    logo: {
-      src: 'https://github.githubassets.com/images/modules/logos_page/Octocat.png',
-    },
-  }).toCanvas();
+    logo: './src/Octocat.jpg',
+  })
+    .toCanvas()
+    .catch(err =>
+      console.log(
+        'There has been a problem with code generation: ' + err.message
+      )
+    );
 });
