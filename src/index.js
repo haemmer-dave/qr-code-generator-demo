b85d08a7fcc6c1da0c343871a46a2a508c4a4193
David Hämmerle <12235946+haemmer-dave@users.noreply.github.com>
2021-10-17 20:55:05 +0200

    Adding CSS and error handling

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
 import QrCodeWithLogo from 'qrcode-with-logos';
 
 const qrContent = document.getElementById('qr-content');
+const imageUrl = document.getElementById('qr-logo');
 const canvas = document.getElementById('qr-output');
 const submitButton = document.getElementById('generate-button');
+const inputError = document.querySelector('span.error');
+
+qrContent.setCustomValidity('');
 
 submitButton.addEventListener('click', function (event) {
+  let context = canvas.getContext('2d');
+  context.clearRect(0, 0, canvas.width, canvas.height);
+
+  if (qrContent.validity.valueMissing) {
+    inputError.textContent = 'Please enter a value!';
+    inputError.className = 'error active';
+    return;
+  } else {
+    inputError.textContent = '';
+    inputError.className = 'error';
+  }
+
   let qrCode = new QrCodeWithLogo({
     canvas: canvas,
     content: qrContent.value,
-    width: 380,
-    image: document.getElementById('image'),
-    logo: './src/Octocat.jpg',
+    width: 400,
+    logo: imageUrl.value,
   })
     .toCanvas()
     .catch(err =>
